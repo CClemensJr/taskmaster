eb91c0cb773e05da92f8053bcf4e06f9c8f34140
Charles Clemens <cclemensjr@gmail.com>
2019-07-02 13:31:26 -0700

    Updated the repo

--- src/main/java/com/clmen/taskmaster/configuration/DynamoDBConfig.java
+++ src/main/java/com/clmen/taskmaster/configuration/DynamoDBConfig.java
@@ -1,4 +1,8 @@
 package com.clmen.taskmaster.configuration;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
 public class DynamoDBConfig {
 }
--- src/main/java/com/clmen/taskmaster/repositories/TaskRepository.java
+++ src/main/java/com/clmen/taskmaster/repositories/TaskRepository.java
@@ -1,4 +1,12 @@
 package com.clmen.taskmaster.repositories;
 
-public class TaskRepository {
+import com.clmen.taskmaster.models.Task;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+@EnableScan
+public interface TaskRepository extends CrudRepository<Task, String> {
+    Optional<Task> findById(String id);
 }
