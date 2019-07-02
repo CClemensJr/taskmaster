fd391d5cf857bfc19c3004bbd9d7c3084943fc13
Charles Clemens <cclemensjr@gmail.com>
2019-07-02 13:26:38 -0700

    Added fields and methods to Task model

--- src/main/java/com/clmen/taskmaster/models/Task.java
+++ src/main/java/com/clmen/taskmaster/models/Task.java
@@ -1,4 +1,48 @@
 package com.clmen.taskmaster.models;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.UUID;
+
+@DynamoDBTable(tableName = "Tasks")
 public class Task {
+/************
+ * Instance Variables
+ * */
+    private UUID id;
+    private String title;
+    private String description;
+    private Status status;
+
+/************
+ * Constructors
+ * */
+    public Task() {}
+
+    public Task(String t, String d) {
+        this.title = t;
+        this.description = d;
+        this.status = Status.Available;
+    }
+
+
+/************
+ * Getters/Setters
+ * */
+    public UUID getId() { return id; }
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public Status getStatus() { return status; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setDescription(String description) { this.description = description; }
+    public void setStatus(Status status) { this.status = status; }
+
+
+/************
+ * Enum
+ * */
+    public enum Status {
+        Available, Assigned, Accepted, Finished
+    }
 }
