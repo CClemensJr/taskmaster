a86124e2acc2959c37f726e52b9acc0210ef3c7d
Charles Clemens <cclemensjr@gmail.com>
2019-07-09 14:19:27 -0700

    Updated repo

--- src/main/java/com/clmen/taskmaster/repositories/TaskRepository.java
+++ src/main/java/com/clmen/taskmaster/repositories/TaskRepository.java
@@ -5,9 +5,13 @@ import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @EnableScan
-public interface TaskRepository extends CrudRepository<Task, String> {
-    Optional<Task> findById(String id);
+public interface TaskRepository extends CrudRepository<Task, Integer> {
+    @EnableScan
+    Iterable<Task> findAll();
+
+    Optional<Task> findById(UUID id);
 }
 
